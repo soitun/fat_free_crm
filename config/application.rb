9fa791b1893a151dbc9f1833e1e240d3a49c89e6
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-04-12 02:44:53 +0000

    Drop back to 7.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ require 'fat_free_crm/gem_ext/rails/engine'
 module FatFreeCRM
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
